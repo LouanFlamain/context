540df4a025e3052e1d28788a3813f763371c4ecc
LouanFlamain <flamainlouan@gmail.com>
2022-11-15 14:25:36 +0100

    add mobile V

--- src/component/header.js
+++ src/component/header.js
@@ -1,57 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <nav
-      className="navbar has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-          />
-        </a>
+  const [status, setStatus] = useState(false);
+  const activate = () => {
+    if (!status) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+    console.log(status);
+  };
+  const isOpen = () => {
+    setStatus(false);
+  };
+  if (!status) {
+    return (
+      <nav
+        className="navbar has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width="112"
+              height="28"
+            />
+          </a>
 
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
-
-          <Link to="/color" className="navbar-item">
-            Color
-          </Link>
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={activate}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
 
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">Log in</a>
-            </div>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item" onClick={isOpen}>
+              Home
+            </Link>
+
+            <Link to="/color" className="navbar-item" onClick={isOpen}>
+              Color
+            </Link>
           </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  } else {
+    return (
+      <nav
+        className="navbar has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width="112"
+              height="28"
+            />
+          </a>
+
+          <a
+            role="button"
+            className="navbar-burger is-active"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={activate}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu is-active">
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item" onClick={isOpen}>
+              Home
+            </Link>
+
+            <Link to="/color" className="navbar-item" onClick={isOpen}>
+              Color
+            </Link>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
